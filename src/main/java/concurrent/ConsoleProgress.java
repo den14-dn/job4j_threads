fda1d1028eddc1e3e5f49dfbbe64105dd4fd9b98
den14-dn <den14-dn@yandex.ru>
2022-06-15 16:23:15 +0300

    3. Прерывание нити [#1019 #312711]

--- src/main/java/concurrent/ConsoleProgress.java
+++ src/main/java/concurrent/ConsoleProgress.java
@@ -1,10 +1,10 @@
 package concurrent;
 
 public class ConsoleProgress implements Runnable {
-    private String[] process = new String[] {"\\", "|", "/"};
 
     @Override
     public void run() {
+        String[] process = new String[] {"\\", "|", "/"};
         int index = 0;
         while (!Thread.currentThread().isInterrupted()) {
             System.out.print("\r load: " + process[index]);
@@ -13,14 +13,17 @@ public class ConsoleProgress implements Runnable {
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
-            index = ++index < process.length ? index : 0;
+            index++;
+            if (index == process.length) {
+                index = 0;
+            }
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(1000);
+        Thread.sleep(50000);
         progress.interrupt();
     }
 }
