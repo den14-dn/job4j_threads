408d4da092473aa7ede8e77cd48b0e967ee29771
den14-dn <den14-dn@yandex.ru>
2022-06-15 14:43:18 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/concurrent/Wget.java
+++ src/main/java/concurrent/Wget.java
@@ -1,19 +1,52 @@
 package concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    for (int index = 0; index <= 100; index++) {
-                        System.out.print("\rLoading : " + index + "%");
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final String target;
+    private final int speed;
+
+    public Wget(String url, String target, int speed) {
+        this.url = url;
+        this.target = target;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+                 FileOutputStream fileOutputStream = new FileOutputStream(target)) {
+                byte[] dataBuffer = new byte[1024];
+                int bytesRead;
+
+                long leftPoint = System.currentTimeMillis();
+
+                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                    fileOutputStream.write(dataBuffer, 0, bytesRead);
+
+                    long dif = System.currentTimeMillis() - leftPoint;
+                    int speedLoading = (int) (1024 / (dif / 1000));
+                    if (speedLoading < speed) {
+                        Thread.sleep((speed - speedLoading) * 1000);
                     }
+                    leftPoint = System.currentTimeMillis();
                 }
-        );
-        thread.start();
+            } catch (Exception e) {
+                Thread.currentThread().interrupted();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        String target = args[1];
+        int speed = Integer.parseInt(args[2]);
+        Thread wget = new Thread(new Wget(url, target, speed));
+        wget.start();
+        wget.join();
     }
 }
