6850710e24524afc1c709cb18226082ea2522556
den14-dn <den14-dn@yandex.ru>
2022-06-15 21:43:57 +0300

    1.1. Состояние нити. [#229175 #312664]

--- src/main/java/concurrent/ThreadState.java
+++ src/main/java/concurrent/ThreadState.java
@@ -8,19 +8,23 @@ public class ThreadState {
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(first.getState());
+        System.out.println("first thread:" + first.getState());
+        System.out.println("second thread:" + second.getState());
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
                 || second.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
+            System.out.println("first thread:" + first.getState());
+            System.out.println("second thread:" + second.getState());
+            System.out.println(second.getState());
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println(first.getState());
+        System.out.println("first thread:" + first.getState());
+        System.out.println("second thread:" + second.getState());
         System.out.println("Job completed");
     }
 }
