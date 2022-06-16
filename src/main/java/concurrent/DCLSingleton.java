b934443282bf584108a5bf3690596860be55922c
den14-dn <den14-dn@yandex.ru>
2022-06-16 10:54:39 +0300

    2. Модель памяти Java [#267917]

--- src/main/java/concurrent/DCLSingleton.java
+++ src/main/java/concurrent/DCLSingleton.java
@@ -1,15 +1,15 @@
 package concurrent;
 
 public class DCLSingleton {
-    private static DCLSingleton inst;
+    /* Для поля inst нужно добавить volatile, чтобы состояние поля было доступно для всех потоков */
+    private static volatile DCLSingleton inst;
 
     public static DCLSingleton instOf() {
-        /* Было дублирование проверки состояния inst,
-        она находилась за пределами блока synchronized,
-        т.о. переменная еще не заблокирована и она может быть в работе у другого потока */
-        synchronized (DCLSingleton.class) {
-            if (inst == null) {
-                inst = new DCLSingleton();
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
             }
         }
         return inst;
